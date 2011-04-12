966edfea9f166c679ce9b067d7c631902683861f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-12 10:57:22 +0100

    build: Squash of allowing a device to generically define its own headers
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   2011-04-12 10:57:22 +0100
        Allow a device to generically define its own headers
    
        We have a few cases of devices including specific versions of projects
        just because of modified headers (msm_mdp.h comes to mind), and I just
        had enough of ifdeffing header files for specific cases (the P990 needs
        a lot of these).
        Now... if a target defines a TARGET_SPECIFIC_HEADER_PATH, any headers in
        there will take precedence over the standard ones; for example, on the
        p990, I have
    
        TARGET_SPECIFIC_HEADER_PATH := device/lge/p990/include
    
        which makes, for example, the
        device/lge/p990/include/hardware_legacy/AudioHardwareInterface.h be
        used instead of
        hardware/libhardware_legacy/include/hardware_legacy/AudioHardwareInterface.h
        whenever a source file uses <hardware_legacy/AudioHardwareInterface.h>
    
        Change-Id: I41b62668b60e3f62a6ebd3738d8d2675103a81e6a
    
        build: fix target header overlay
    
        LOCAL_C_INCLUDES as defined by the makefile should be put AFTER
        the overlay includes so the overlay always takes precedence.
    
        Change-Id: I489b2aab6dbacd9122d834f85e07b63ed1271f07
    
    Author: Jan Altensen <info@stricted.net>
    Date:   2020-09-10 15:18:49 +0200
        soong: add TARGET_SPECIFIC_HEADER_PATH
    
        Change-Id: I582ae8e6e010016b33fd9b020b723e5fc2dc442b
    
    Change-Id: I3c885fe504ce4f646001697119cffd674db9ec49

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -157,6 +157,7 @@ $(call add_json_str,  BtConfigIncludeDir,                $(BOARD_BLUETOOTH_BDROI
 $(call add_json_bool, Device_support_hwfde,              $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_hwfde_perf,         $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_list, DeviceKernelHeaders,               $(TARGET_DEVICE_KERNEL_HEADERS) $(TARGET_BOARD_KERNEL_HEADERS) $(TARGET_PRODUCT_KERNEL_HEADERS))
+$(call add_json_str,  TargetSpecificHeaderPath,          $(TARGET_SPECIFIC_HEADER_PATH))
 $(call add_json_str,  VendorApiLevel,                    $(BOARD_API_LEVEL))
 $(call add_json_str,  VendorApiLevelPropOverride,        $(BOARD_API_LEVEL_PROP_OVERRIDE))
 $(call add_json_list, ExtraVndkVersions,                 $(PRODUCT_EXTRA_VNDK_VERSIONS))
