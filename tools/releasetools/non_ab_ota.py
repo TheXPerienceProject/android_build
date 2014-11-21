b4ee5b3a38e2307e1da2e41000b150b4da769103
Matt Mower <mowerm@gmail.com>
2014-11-21 00:46:52 -0600

    build: Ensure /system unmounted at install start
    Account for the possibility that a user has mounted system manually
    before installing the FullOTA package.
    
    Change-Id: I97d0540676a7ce98762545e76ecb1a7f6335d204
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -234,7 +234,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("  \ \/ / _ \ __| _ \_ _| __| \| ((/ __| __|  ");
   script.Print("   >  <|  _/ _||   /| || _|| .` || (__| _|   ");
   script.Print("  /_/\_\_| |___|_|_\___|___|_|\_| \___|___|  ");
-  script.Print("     (C)2011-2022 The XPerience Project      ");
+  script.Print("     (C)2011-2024 The XPerience Project      ");
   script.Print("           By Carlos 'Klozz' Jesus           ");
   script.Print("           TeamMEX@xda-developers            ");
   script.Print("----------------------------------------------");
@@ -248,6 +248,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" Device Status: %s"%(device_status));
   script.Print("----------------------------------------------");
 
+  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
