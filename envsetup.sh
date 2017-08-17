0e6086a672d9fb9c36600d085dfd5d1d8e233467
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add XPerience build support
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include XPerience pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use XPerience pathmap
    
        even if we're not building a XPerience target, we need to
        use the XPerience pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    Signed-off-by: klozz <klozz@vivaldi.net>

--- envsetup.sh
+++ envsetup.sh
@@ -138,6 +138,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^xperience_") ; then
+        XPERIENCE_BUILD=$(echo -n $1 | sed -e 's/^xperience_//g')
+    else
+        XPERIENCE_BUILD=
+    fi
+    export XPERIENCE_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -647,6 +654,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
