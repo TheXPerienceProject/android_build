2645a079307611efbe7f38caba18c4131becc20d
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add XPerience build support
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include XPerience pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use XPerience pathmap
    
        even if we're not building a XPerience target, we need to
        use the aosp pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Fri Aug 18 10:17:00 2017 +0200
    
        envsetup: Call fixup_common_out_dir
    
      Change-Id: I84e16b0df5822cfd8514c46d709109a13c20f86e
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -474,6 +474,10 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(XPERIENCE_BUILD),)
+include vendor/xperience/config/BoardConfigXPerience.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1240,6 +1244,12 @@ endif
     TARGET_ODM_PROP \
 
 include $(BUILD_SYSTEM)/sysprop_config.mk
+
+ifneq ($(XPERIENCE_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/xperience/sepolicy/common/sepolicy.mk)
+endif
 
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
