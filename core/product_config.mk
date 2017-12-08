0d2c57af693bc936a012f9e17acdf4fe41896f48
klozz <klozz707@gmail.com>
2017-12-08 02:55:06 -0500

    XPerienced! since im a lazy guy I kanged build files from LOS x_x
    Change-Id: I3e80ead06d2bde180c35508b7cd4fd606ce13c3e
    Signed-off-by: klozz <klozz707@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A xperience build needs only the xperience product makefiles.
+ifneq ($(XPE_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(XPE_BUILD)/xpe.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # XPE_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(XPE_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
