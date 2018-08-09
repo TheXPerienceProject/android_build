778d98740b8d722b77f21a493e8c3479af62fb55
klozz <shaclone707@gmail.com>
2018-08-09 03:53:58 -0400

    dont call our sepolicy for now
    Change-Id: I7500db8a46ec3613b4ee14d3373fc79e9c3c096d
    Signed-off-by: klozz <shaclone707@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1095,7 +1095,7 @@ endif
 ifneq ($(XPE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/xperience/sepolicy/common/sepolicy.mk)
+#$(eval include device/xperience/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
