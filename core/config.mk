e2bd9ad0323b9108c2c4ce5178bfa4fad81ec795
klozz <shaclone707@gmail.com>
2018-08-13 04:06:08 -0400

    include QCOM/MTK targets
    Change-Id: I15089c6a3a23c2adda3e83219ebf253b327ec03f
    Signed-off-by: klozz <shaclone707@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1251,4 +1251,10 @@ endif
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
 
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/xperience/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/xperience/build/core/mtk_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
