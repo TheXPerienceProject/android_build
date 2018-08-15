be871f772ed93e11df36087054b1875d825d7191
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2018-08-14 22:29:16 -0500

    OTA: xperience_version is generated now move from printing android_version to xperience_version

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -843,7 +843,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("          TeamMEX@xda-developers            ");
   script.Print("----------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
-  script.Print(" XPerience version: %s"%(android_version));
+  script.Print(" XPerience version: %s"%(xperience_version));
   script.Print(" Build id: %s"%(build_id));
   script.Print(" Build date: %s"%(build_date));
   script.Print(" Security patch: %s"%(security_patch));
