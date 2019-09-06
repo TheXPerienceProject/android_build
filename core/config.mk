610c882d30c6f0a0d55d9b26ffc1b5a7abce4db3
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include XPerience SEpolicy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/config.mk
+++ core/config.mk
@@ -1303,6 +1303,14 @@ endif
 
 include $(BUILD_SYSTEM)/sysprop_config.mk
 
+ifneq ($(XPERIENCE_BUILD),)
+ifneq ($(wildcard device/xperience/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/xperience/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
