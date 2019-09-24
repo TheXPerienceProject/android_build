67f229783a85b6261f7a8d0db9ba8b3785728a65
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 18:45:17 +0200

    releasetools: Move system mount handling to backuptool
    * This allows the ROM to be flashed on any recovery, whether it mounts
      system to /system, /system_root or /mnt/system like Q recovery
    
    * Remove any type of system mount with static paths from updater-script
      to figure out the path dinamically through backuptool
    
    Change-Id: I7b33726305cb6f3eec146d26135f0081002a25dc
    Signed-off-by: klozz <klozz@vivaldi.net>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -992,7 +992,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" Model: %s"%(model));
   script.Print(" Device Status: %s"%(device_status));
   script.Print("----------------------------------------------");
-  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
@@ -1000,10 +999,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
+  if target_info.get("system_root_image") == "true":
+    sysmount = "/"
+  else:
+    sysmount = "/system"
+
   if OPTIONS.backuptool:
-    script.Mount("/system")
-    script.RunBackup("backup")
-    script.Unmount("/system")
+    script.RunBackup("backup", sysmount)
 
   system_progress = 0.75
 
@@ -1066,9 +1068,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
-    script.Mount("/system")
-    script.RunBackup("restore")
-    script.Unmount("/system")
+    script.RunBackup("restore", sysmount)
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
