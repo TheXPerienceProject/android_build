970ef936854256510b49ba4fc0f0341ddea448fd
klozz <klozz@TheXPerienceProject.org>
2021-03-08 05:53:23 +0100

    Don't build ancient browser2 and quicksearchbox
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -23,7 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Browser2 \
     Calendar \
     Camera2 \
     Contacts \
@@ -33,7 +32,6 @@ PRODUCT_PACKAGES += \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
-    QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
