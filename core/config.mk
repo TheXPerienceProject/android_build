CarbonROM/android_build_make

build: Move sepolicy inclusion in front of buildsystem inclusions
Change-Id: I3ad898c5427960deecc48c8c51358301678534f8

core/config.mk
@@ -1232,6 +1232,12 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
+ifneq ($(CARBON_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/carbon/sepolicy/common/sepolicy.mk)
+endif
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
@@ -1241,12 +1247,6 @@ endif
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
-ifneq ($(CARBON_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/carbon/sepolicy/common/sepolicy.mk)
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk

TheXPerienceProject/android_build

Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

core/config.mk
@@ -1232,6 +1232,13 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
+
+ifneq ($(XPERIENCE_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/xperience/sepolicy/common/sepolicy.mk)
+endif
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
@@ -1241,14 +1248,6 @@ endif
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
-ifneq ($(XPERIENCE_BUILD),)
-ifneq ($(wildcard device/xperience/sepolicy/common/sepolicy.mk),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/xperience/sepolicy/common/sepolicy.mk)
-endif
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk