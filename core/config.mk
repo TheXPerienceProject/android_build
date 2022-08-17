5f54a6005e9dd3b7860ee018623f51fe1e65c119
klozz <carlosj@klozz.dev>
2022-08-17 21:20:48 +0200

    config: fixup! dumpar was removed by error on https://github.com/TheXPerienceProject/android_build/commit/ad7baf3c7b6748205ec246babfae6881629241dc
    Change-Id: I194e79e926a00e7e0ae27014b0bf1c5d3bc41bbf
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/config.mk
+++ core/config.mk
@@ -410,6 +410,7 @@ endif
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+
 ifneq ($(XPERIENCE_BUILD),)
 include vendor/xperience/config/BoardConfigXPerience.mk
 endif
@@ -1329,3 +1330,5 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
+
+include $(BUILD_SYSTEM)/dumpvar.mk
