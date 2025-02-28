b97cb7d8ed05fc72fd39491c14b81c489dc292ce
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-28 11:04:37 +0800

    build: Erase resource-cache upon flashing
    Change-Id: Ib29bf521f7ff662287ced028d22eb1d85aee0c23

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3688,6 +3688,9 @@ class BlockDifference(object):
     call = ('delete_recursive("/data/system/package_cache");')
     script.AppendExtra(script.WordWrap(call))
 
+    call = ('delete_recursive("/data/resource-cache");')
+    script.AppendExtra(script.WordWrap(call))
+
   def _HashBlocks(self, source, ranges):  # pylint: disable=no-self-use
     data = source.ReadRangeSet(ranges)
     ctx = sha1()
