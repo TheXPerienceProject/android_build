c128bc9b35c93b079b866ef6d622732dcd091a02
klozz <carlosj@klozz.dev>
2026-02-20 16:07:32 -0600

    dont build apn's on GSI
    Change-Id: Ifc88ba2b0d8eb3df413efad22e189adefd87e6e5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -28,5 +28,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(XPERIENCE_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
